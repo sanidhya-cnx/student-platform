9bc8b6c7ebe9243881c501cb395d2d538437815b
Anuragp-0407 <pandeyanurag0407@gmail.com>
2026-03-14 23:49:18 +0530

    Implemented login/register UI, authentication logic and protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,49 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Projects from "./pages/Discover"
 import Dashboard from "./pages/Dashboard"
 import CreateProject from "./pages/CreateProject"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 function App() {
-  
 
   return (
-    <>
     <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/discover" element={<Projects />} />
-      <Route path="/project/create" element={<CreateProject />} />
+
+      <Route path="/" element={<Navigate to="/login" />} />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/discover"
+        element={
+          <ProtectedRoute>
+            <Projects />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/project/create"
+        element={
+          <ProtectedRoute>
+            <CreateProject />
+          </ProtectedRoute>
+        }
+      />
+
     </Routes>
-    </>
   )
 }
 
